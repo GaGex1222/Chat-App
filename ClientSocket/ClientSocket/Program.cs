7e8759181f836521f6110f4121bddf2b1e240915
Gal <gald12123434@gmail.com>
2024-11-10 22:45:00 +0200

    Initial Commit

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string username;
+            do
+            {
+                Console.WriteLine("Please enter a username: ");
+                username = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(username));
+            // Set the TcpClient
+            string server = "127.0.0.1";
+            int port = 13000;
+
+            TcpClient client = new TcpClient(server, port);
+            NetworkStream stream = client.GetStream();
+            Byte[] displayUsername = Encoding.ASCII.GetBytes(username);
+
+            stream.Write(displayUsername, 0, displayUsername.Length);
+
+
+            // Translate the passed message into ASCII and store it as a Byte array
+            while (true)
+            {
+                Console.WriteLine("Type A message to send to the server");
+                String userMessage = Console.ReadLine();
+                if (userMessage == "end")
+                {
+                    break;
+                }
+
+                string usernameAndMessage = $"{username} {userMessage}";
+                Byte[] data = Encoding.ASCII.GetBytes(usernameAndMessage);
+                
+
+                // Send the message to the connected TcpServer
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", usernameAndMessage
+                    );
+
+                // Buffer to store the response bytes
+                data = new Byte[256];
+
+                // String to store the response ASCII representation
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+            }
+            Console.WriteLine("Connection ended!");
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+    }
+}
---
+++ Server/App.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace CHATAPP
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e); // Call the base OnStartup to properly start the WPF application
+
+            // Start the server on a separate thread
+            Thread serverThread = new Thread(StartServer);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        private void addMessageToUI(string message)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var mainWindow = Application.Current.MainWindow as MainWindow;
+                if (mainWindow != null)
+                {
+                    mainWindow.TextMessagesLines.Items.Add(message);
+                }
+            });
+        }
+
+        private string[] seperateUsernameAndMessage(string item)
+        {
+            string[] splittedString = item.Split(' ');
+            string username = splittedString[0];
+            string messageToShow = string.Join(' ', splittedString.Skip(1));
+            string[] usernameAndMessageSeperated = { messageToShow, username };
+            return usernameAndMessageSeperated;
+        }
+
+        private void handleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            string username = null;
+            NetworkStream stream = client.GetStream();
+            Byte[] bytes = new Byte[256];
+            string data = null;
+            int i;
+
+            try
+            {
+                while (true)
+                {
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to an ASCII string
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+
+                        // Send back a response to the client
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                        if (data.Split(' ').Length > 1)
+                        {
+                            string[] usernameAndMessageSeperated = seperateUsernameAndMessage(data);
+                            addMessageToUI($"{usernameAndMessageSeperated[0]}: {usernameAndMessageSeperated[1]}");
+                        }
+                        else
+                        {
+                            username = data;
+                            addMessageToUI($"{username} has connected to the server!");
+                        }
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                addMessageToUI("SocketException");
+            }
+            catch (IOException ex)
+            {
+                addMessageToUI($"{username} have disconnected!");
+            }
+            finally
+            {
+                client.Close();
+            }
+
+        }
+
+        private void StartServer()
+        {
+            int port = 13000;
+            TcpListener server = null;
+
+            try
+            {
+                // Initialize TcpListener
+                server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Thread handlingClientThread = new Thread(handleClient);
+                    handlingClientThread.IsBackground = true;
+                    handlingClientThread.Start(client);
+                }
+            }
+            catch (SocketException ex)
+            {
+                addMessageToUI("SocketException");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
